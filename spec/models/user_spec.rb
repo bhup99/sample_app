ec1855b9155a3462fd4891c486b62476986caf8e
Bhupesh Kumar <bhupeshkumar99@gmail.com>
2014-07-23 06:00:12 +0530

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,11 @@ describe User do
 	it { should_not be_admin }
 	it { should respond_to(:blogs) }
 	it { should respond_to(:feed) }
+	it { should respond_to(:relationships) }
+	it { should respond_to(:followed_users) }
+	it { should respond_to(:following?) }
+	it { should respond_to(:follow!) }
+	it { should respond_to(:unfollow!) }
 
 
 	describe "with admin attribute set to 'true'" do
@@ -141,10 +146,44 @@ describe User do
 			let(:unfollowed_post) do
 				FactoryGirl.create(:blog, user: FactoryGirl.create(:user))
 			end
+			let(:followed_user) { FactoryGirl.create(:user) }
+
+			before do
+				@user.follow!(followed_user)
+				3.times { followed_user.blogs.create!(content: "Fuck that shit and fuck all!") } 
+			end
 
 			its(:feed) { should include(newer_blog) }
 			its(:feed) { should include(older_blog) }
 			its(:feed) { should_not include(unfollowed_post) }
+			its(:feed) do
+				followed_user.blogs.each do |blog|
+					should include(blog)
+				end
+			end
+		end
+	end
+
+	describe "following" do
+		let(:other_user) { FactoryGirl.create(:user) }
+		before do
+			@user.save
+			@user.follow!(other_user)
+		end
+
+		it { should be_following(other_user) }
+		its(:followed_users) { should include(other_user) }
+
+		describe "followed user" do
+			subject { other_user }
+			its(:followers) { should include(@user) }
+		end
+
+		describe "and unfollowing" do
+			before { @user.unfollow!(other_user) }
+
+			it { should_not be_following(other_user) }
+			its(:followed_users) { should_not include(other_user) }
 		end
 	end
 end
